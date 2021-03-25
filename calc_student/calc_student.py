79673f29b179a7b307b9df769ff09ab18fa855de
Mayra Ochoa <mayra@Admins-MacBook-Pro.local>
2021-03-25 14:50:50 -0700

    Project 2

---
+++ calc_student/calc_student.py
@@ -0,0 +1,27 @@
+#import library
+
+#create an instance of TK()
+
+# add a title for window
+
+#create an Entry instance for user input
+
+
+#e.insert(0, "")
+
+# Define functions
+    #button_click(number)
+    #button_add()
+    #button_equal()
+    #button_clear()
+
+
+#Define Buttons
+
+
+# Put Buttons on the screen
+    # i.e. button_1.grid(row=3, column=0)
+
+
+
+# Add mainloop() method
